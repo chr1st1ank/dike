978cfb03d6d7b8e3b3e2ee82639dfadae8e87985
chr1st1ank <chr1st1ank@krudewig-online.de>
2021-07-07 23:46:09 +0200

    Tests for wrap_in_coroutine

---
+++ tests/test_wrap_in_coroutine.py
@@ -0,0 +1,64 @@
+"""Tests for the decoweb.wrap_in_coroutine decorator"""
+import asyncio
+
+from decoweb import wrap_in_coroutine
+
+
+def test_wrap_function():
+    """Wrap a function without arguments"""
+
+    @wrap_in_coroutine
+    def f():
+        return "X"
+
+    assert asyncio.run(f()) == "X"
+
+
+def test_wrap_function_args():
+    """Wrap a function with positional arguments"""
+
+    @wrap_in_coroutine
+    def f(*args):
+        return args
+
+    assert asyncio.run(f("A", 2)) == ("A", 2)
+
+
+def test_wrap_function_kargs():
+    """Wrap a function with keyword arguments"""
+
+    @wrap_in_coroutine
+    def f(**kwargs):
+        return kwargs
+
+    assert asyncio.run(f(a="A", b=2)) == dict(a="A", b=2)
+
+
+def test_wrap_coroutine():
+    """Wrap a coroutine without arguments"""
+
+    @wrap_in_coroutine
+    async def f():
+        return "X"
+
+    assert asyncio.run(f()) == "X"
+
+
+def test_wrap_coroutine_args():
+    """Wrap a coroutine with positional arguments"""
+
+    @wrap_in_coroutine
+    async def f(*args):
+        return args
+
+    assert asyncio.run(f("A", 2)) == ("A", 2)
+
+
+def test_wrap_coroutine_kargs():
+    """Wrap a coroutine with keyword arguments"""
+
+    @wrap_in_coroutine
+    async def f(**kwargs):
+        return kwargs
+
+    assert asyncio.run(f(a="A", b=2)) == dict(a="A", b=2)
